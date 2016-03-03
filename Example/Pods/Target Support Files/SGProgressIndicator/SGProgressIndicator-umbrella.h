82df89f2e946ccc94cd456560020f9261adefff1
SALVATORE <s.graf@italwork.com>
2016-03-03 12:08:31 +0100

    Update

--- Example/Pods/Target Support Files/SGProgressIndicator/SGProgressIndicator-umbrella.h
+++ Example/Pods/Target Support Files/SGProgressIndicator/SGProgressIndicator-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "SGProgressIndicator.h"
 
 FOUNDATION_EXPORT double SGProgressIndicatorVersionNumber;
 FOUNDATION_EXPORT const unsigned char SGProgressIndicatorVersionString[];
